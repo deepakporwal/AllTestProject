c381ec941d1522a5893fd825ced408178a6a2e5b
deepakporwal <deepakporwal@deepak-vm>
2023-02-24 17:29:38 +0000

    Add project files.

---
+++ TestingProjects/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestingProjects
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
